15bd1dbe037c3a1c615a8f96eecee7a25b5fdcae
Sai Rohit <sairohitp007@gmail.com>
2023-11-10 16:03:04 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./assets/logo.png";
 
 function App() {
     return (
         <nav class="navbar">
             <div className="subbody">
+                <div>
+                    <img src={logo} />
+                </div>
+                <div>
                 <a href="#">HOME</a>
                 <a href="#">College Dakhla</a>
                 <a href="#">ADMISSION</a>
                 <a href="#">ABOUT US</a>
                 <a href="#">COURSES</a>
-                <a href="#">MOUS</a>
+                <a href="#">MBBS</a>
                 <a href="#">CONTACT US</a>
-                <a href="#">CALL +918898880100</a>
+                </div>
+                <div>
+                    <a href="#">CALL +918898880100</a>
+                </div>
             </div>
         </nav>
     );
