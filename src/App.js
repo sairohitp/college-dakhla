c3f41ab0fc73856ef7a6af042bbf7509e379b0fa
Sai Rohit <sairohitp007@gmail.com>
2023-11-10 15:53:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     <nav class="navbar">
-      <a href="#">HOME</a>
-      <a href="#">College Dakhla</a>
-      <a href="#">ADMISSION</a>
-      <a href="#">ABOUT US</a>
-      <a href="#">COURSES</a>
-      <a href="#">MOUS</a>
-      <a href="#">CONTACT US</a>
-      <a href="#">CALL +918898880100</a>
-    </nav>
-
-    </div>
-  );
+    return (
+        <nav class="navbar">
+            <div className="subbody">
+                <a href="#">HOME</a>
+                <a href="#">College Dakhla</a>
+                <a href="#">ADMISSION</a>
+                <a href="#">ABOUT US</a>
+                <a href="#">COURSES</a>
+                <a href="#">MOUS</a>
+                <a href="#">CONTACT US</a>
+                <a href="#">CALL +918898880100</a>
+            </div>
+        </nav>
+    );
 }
 
 export default App;
